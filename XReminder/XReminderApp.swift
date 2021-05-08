7ee40eeb9b80d1e993a0e7475b9e6d4898466c32
Emile Wong <emilewong@outlook.com>
2021-05-08 18:13:40 +0800

    Initial Commit

---
+++ XReminder/XReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  XReminderApp.swift
+//  XReminder
+//
+//  Created by Emile Wong on 8/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct XReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
